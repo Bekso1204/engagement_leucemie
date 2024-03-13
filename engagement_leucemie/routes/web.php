91d59d8e43339e59d05dfcb807671eae2661401b
Yohanlng <yohan.languille@gmail.com>
2024-03-13 09:00:58 +0100

    conflit

--- engagement_leucemie/routes/web.php
+++ engagement_leucemie/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/template', function () {
+    return view('template');
+
+Route::get('/contact', 'ContactController@showForm')->name('contact.form');
+Route::post('/contact', 'ContactController@submitForm')->name('contact.submit');
+
+Route::get('/presentation', 'PresentationController@show')->name('presentation.show');
