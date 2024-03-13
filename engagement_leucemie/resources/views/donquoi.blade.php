d2ba388e6fbb314548158c6762101ac9ea5c0c71
HotDogNoir <121878437+HotDogNoir@users.noreply.github.com>
2024-03-13 08:08:35 +0100

    Pages fixe bientôt prête, il faudra parcontre le bootstrap

---
+++ engagement_leucemie/resources/views/donquoi.blade.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Don de Moelle Osseuse</title>
+
+</head>
+
+<body>
+<!-- Titre Don de Moelle Osseuse -->
+    <h1>Don de Moelle Osseuse</h1>
+<!-- Paragraphe 1 -->
+    <p>
+
+        La greffe de moelle osseuse représente un espoir de guérison pour de nombreuses personnes atteintes de cancers ou de maladies graves du système sanguin. La moelle osseuse, source des cellules du sang. La moelle osseuse est située dans les os longs et les os plats - à ne pas confondre avec la moelle épinière, située dans la colonne vertébrale. Elle renferme des cellules très précieuses, appelées cellules souches hématopoïétiques, qui donnent naissance aux cellules du sang : les globules rouges, les globules blancs et les plaquettes.
+
+    </p>
+<!-- Titre à quoi sert le don ?-->
+    <h3>A quoi sert le don?</h3>
+<!-- Paragraphe 2 -->
+    <p>
+
+        Pour soigner les maladies du sang, la greffe de moelle osseuse est l'un des traitements efficaces.80 % des greffes de moelle osseuse sont réalisées pour traiter certaines formes de cancer comme les leucémies, les lymphomes, les myélomes... Les personnes atteintes de maladie du sang (thalassémie, drépanocytose), d'anémie aplasique ou de troubles héréditaires du système immunitaire et du métabolisme peuvent également avoir besoin d'une greffe de moelle osseuse.
+    </p>
+<!-- Titre Qui peut donner -->
+    <h3>Qui peut donner?</h3>
+<!-- Paragraphe 3 -->
+    <p>
+
+        Être volontaire au don de moelle osseuse signifie être inscrit sur le registre France greffe de moelle. <br>
+        Pour ce faire, il faut :
+
+    </p>
+
+    <ul>
+
+        <li>
+
+            être en parfaite santé et avoir entre 18 et moins de 36 ans lors de l'inscription, même si on peut donner jusqu'à la veille de ces 61 ans ;
+        </li>
+
+        <li>
+
+            passer un entretien médical spécifique pour faire le point sur ses antécédents médicaux et son mode de vie ;
+
+        </li>
+
+        <li>
+
+            s'engager à rester joignable et à se rendre disponible pour les examens médicaux en cas de compatibilité avec un receveur.
+
+        </li>
+
+    </ul>
+
+<!-- Emplacement vidéo -->
+
+<!-- Paragraphe 4 -->
+La compatibilité nécessaire pour une greffe de moelle osseuse fait appel à un système complexe, le HLA, différent de celui des groupes sanguins. La probabilité de trouver deux individus compatibles pour une arête de moelle osseuse en dehors de la fratrie est ainsi d'une sur un million ! Sachant que pour deux membres d'une même fratrie, les probabilités s'élèvent à une sur quatre. C'est pourquoi chaque nouvelle inscription compte et apporte une chance supplémentaire de guérison pour les malades.</p>
+<!-- Titre Le Déroulement-->
+<h3>Le Déroulement</h3>
+
+<!-- Case 1 -->
+<!-- Image -->
+<!-- Titre Avant le Don-->
+<h3>Avant le Don</h3>
+
+<!-- Paragraphe 5 -->
+
+<!-- Case 2 -->
+<!-- Image -->
+<!-- Titre Le prévèlement-->
+<h3>Le prélèvement</h3>
+
+<!-- Paragraphe 6 -->
+
+<!-- Case 3 -->
+<!-- Image -->
+<!-- Titre Après le Don-->
+<h3>Après le Don</h3>
+
+<!-- Paragraphe 7 -->
+
+<!-- Titre Pour aller plus loin -->
+<h3>Pour aller plus loin</h3>
+
+<!-- Surement une case pour ranger les vidéos -->
+
+<!-- Emplacement Vidéo -->
+
+<!-- Emplacement Vidéo -->
+
+<!-- Emplacement Vidéo -->
+
+<!-- Emplacement Vidéo -->
+
+<!-- Titre Encore Plus -->
+<h3>Liens Externes</h3>
+
+
+<!-- Lien 1 -->
+
+<!-- Lien 2 -->
+
+<!-- Lien 3 -->
+
+
+
+</body>
+
+</html>
---
+++ engagement_leucemie/resources/views/limiteage.blade.php
@@ -0,0 +1,8 @@
+<!-- Titre Pourquoi une limite d'âge -->
+<!-- Titre texte -->
+<!-- Vidéo -->
+<!-- Tableau -->
+<!-- Titre Quel est l'argumentaire -->
+<!-- Paragraphe 1 -->
+<!-- Paragraphe 2 -->
+<!-- Paragraphe 3 -->
