07ead2a7d6c7e84fbdff510f7c4e9450715b625b
Alexian <alexian.pellier2912@gmail.com>
2024-03-16 14:41:17 +0100

    add back-office

---
+++ back_office/app/Models/Fonction.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Fonction extends Model
+{
+    use HasFactory;
+
+    public function bureau():BelongsToMany{
+        return $this->belongsToMany(Bureau::class);
+    }
+}
